8e974f0005beae62c09b36b2014a1ef01cade359
aymenqamhan <aymengethub00@gmail.com>
2026-01-02 02:27:44 +0300

    Remove Supabase key from source code
    Replaced the Supabase key with an empty string for security.

--- team-work/src/api/supabase.js
+++ team-work/src/api/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = "https://syfhfrduueesvtimkkyt.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5ZmhmcmR1dWVlc3Z0aW1ra3l0Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1Nzg2NjQ0MCwiZXhwIjoyMDczNDQyNDQwfQ.rLs96NYTvvsIWE-iW0Uh9p8iBG3jLx_Vf1GkXnZqFx4";
+const supabaseKey = ""; // secret key from supabase
 
 if (!supabaseUrl || !supabaseKey) {
     console.error("Supabase variables are missing in .env");
